ea87fb8a8edcf6595557635342425a3a49d00ed0
Elissa McLaren <emclaren@bastiansolutions.com>
2019-04-23 16:23:02 -0400

    Probs won't work

--- SmashRankingAPI/Resources/Sets/socketEvents.js
+++ SmashRankingAPI/Resources/Sets/socketEvents.js
@@ -26,35 +26,60 @@ module.exports = (io) => {
             
         }
 
-        joinSet = (set) => {
-            player = {id: socket.user.id};
+        joinSet = async (set) => {
+            set = {};
             socket.join(set.setId);
 
             console.log("Joining set");
             setRef = db.collection("Events").doc(set.eventId).collection("Sets").doc(set.setId);
             setRef.update({
                 players: admin.firestore.FieldValue.arrayUnion(socket.user.id)
-            }).then(() => {
-                db.collection('Users').doc(player.id).collection("Ranks").get()
-                    .then(snapshot => {
-                        ranks = [];
-                        snapshot.docs.map(doc => {
-                            rank = doc.data()
-                            rank.id = doc.id;
-                            ranks.push(rank);
-                        });
-                        player.ranks = ranks;
-                        io.to(set.setId).emit("setJoined", player);
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        //Error handing
-                    });
-                
-            })
-            .catch((err) => {
-                console.log(err);
             });
+            setRef.get().then(doc => {
+                doc.players.forEach(player => {
+                    db.collection("Users").doc(player).collection("Ranks").get()
+                        .then(() => {
+                            console.log("Getting player ", player, " rankings");
+                            ranks = ranks.docs.map(doc => {
+                                var newRank = doc.data();
+                                newRank.id = doc.id;
+                                return newRank;
+                            });
+                        })
+                        .catch(err => console.log(err));
+                        console.log("Deciding if joiner or creator");
+                        if (player == socket.user.id) {
+                            set.joiner = {id: socket.user.id, ranks};
+                        }
+                        else {
+                            set.creator = {ranks};
+                        }
+                    });
+
+            })
+            .catch(err => console.log(err));
+                
+                
+                // db.collection('Users').doc(player.id).collection("Ranks").get()
+                //     .then(snapshot => {
+                //         ranks = [];
+                //         snapshot.docs.map(doc => {
+                //             rank = doc.data()
+                //             rank.id = doc.id;
+                //             ranks.push(rank);
+                //         });
+                //         player.ranks = ranks;
+                //         io.to(set.setId).emit("setJoined", player);
+                //     })
+                //     .catch(err => {
+                //         console.log(err);
+                //         //Error handing
+                //     });
+                
+            // })
+            // .catch((err) => {
+            //     console.log(err);
+            // });
             
         }
         removeStage = (set) => {
